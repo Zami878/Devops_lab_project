671cb69c4fdeff9619ea27a76b8d2edb67ee4b69
Zami878 <zamenarizamenari@gmail.com>
2025-01-28 09:17:13 +0530

    done

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import gui 
+import keys
+import encrypting
+import decrypting
+import os
+
+def run_application():
+    
+    gui.create_gui()
+
+if __name__ == "__main__":
+    run_application()  # 
